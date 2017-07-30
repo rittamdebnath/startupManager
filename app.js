c3d9fdc62a03c5f04e6dc28c6e1fc4dbc0aa24b7
Isak La Fleur <isak.engdahl@gmail.com>
2017-07-30 20:35:24 +0200

    fixed the google oauth2.0 api

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const compression = require('compression');
 const path = require('path');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+require('./config/passport');
 const index = require('./routes/index');
-const users = require('./routes/api');
+const api = require('./routes/api');
+const auth = require('./routes/auth');
 
 const app = express();
 app.use(compression(9));
@@ -23,24 +25,25 @@ app.options('*', cors(corsOptions));
 app.use(cors(corsOptions));
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', index);
-app.use('/api', users);
+app.use('/api', api);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/test', function(req, res, next) {
+router.get('/test', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
