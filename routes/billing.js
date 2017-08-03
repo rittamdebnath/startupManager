11bc9d94e9caca974fad4a022575dae71bdc97ca
Isak La Fleur <isak.engdahl@gmail.com>
2017-08-03 20:14:35 +0200

    .

--- routes/billing.js
+++ routes/billing.js
@@ -5,15 +5,11 @@ const requireLogin = require('../middlewares/requireLogin');
 const router = express.Router();
 
 router.post('/stripe', requireLogin, async (req, res) => {
-  // console.log(req.body);
-  if (!req.user) {
-    return res.status(401).send({ error: 'You must login!' });
-  }
   const charge = await stripe.charges.create({
     amount: 500,
     currency: 'usd',
-    source: req.body.id, // obtained with Stripe.js
     description: '$5 for 5 credits',
+    source: req.body.id, // obtained with Stripe.js
   });
   req.user.credits += 5;
   const user = await req.user.save();
